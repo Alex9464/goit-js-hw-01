43bde8137dbd91845285ccb5caa93fdc56075cb8
Alex <redj9464@gmail.com>
2024-08-17 20:40:31 +0300

    tasks mod1

---
+++ js/task-2.js
@@ -0,0 +1,8 @@
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
+}
+
+console.log(getShippingMessage("Australia", 50, 10));
+console.log(getShippingMessage("Germany", 100, 20));
+console.log(getShippingMessage("Sweden", 200, 15));
